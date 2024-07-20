f2bfd4824c314da5e9930df619ff2b03b729bbc9
Matheus Silagi <matsilagi2@hotmail.com>
2024-07-20 01:48:50 -0300

    Grigori and Antlion Grub shouldn't count for points.

--- lua/autorun/server/sv_cstrikemoneysystem.lua
+++ lua/autorun/server/sv_cstrikemoneysystem.lua
@@ -1,6 +1,7 @@
 CreateConVar( "mp_startmoney", "1000" , {FCVAR_REPLICATED,FCVAR_NOTIFY,FCVAR_ARCHIVE} , "Money given at spawn")
 CreateConVar( "mp_npckillreward", "50" , {FCVAR_REPLICATED,FCVAR_NOTIFY,FCVAR_ARCHIVE} , "Money given per killed NPC")
 CreateConVar( "mp_playerkillreward", "300" , {FCVAR_REPLICATED,FCVAR_NOTIFY,FCVAR_ARCHIVE} , "Money given per killed Player")
+CreateConVar( "mp_maxmoney", "16000", {FCVAR_REPLICATED,FCVAR_NOTIFY,FCVAR_ARCHIVE}, "Maximum amount of money a player can have" )
 
 -- NPC Blacklist for point system
 NpcBlacklist = {
@@ -15,33 +16,38 @@ NpcBlacklist = {
 	"npc_kleiner",
 	"npc_crow",
 	"npc_pigeon",
-	"npc_seagull"
+	"npc_seagull",
+	"npc_antlion_grub",
+	"npc_monk"
 }
 
 -- Set money when the player spawns
 hook.Add( "PlayerInitialSpawn", "initialspawnmoneyset", function(ply)
 	
 	local stmne = GetConVar("mp_startmoney"):GetInt()
-	if ply:GetNW2Int("cstrike_money",0) != stmne then
-		ply:SetNW2Int("cstrike_money",stmne)
+	local maxmoney = GetConVar("mp_maxmoney"):GetInt()
+	if ply:GetNW2Int("cstrike_money",0) != math.min(stmne,maxmoney) then
+		ply:SetNW2Int("cstrike_money",math.min(stmne,maxmoney))
 	end
 end)
 
 hook.Add( "PlayerSpawn", "spawnmoneyset", function(ply)
 	
 	local stmne = GetConVar("mp_startmoney"):GetInt()
-	if ply:GetNW2Int("cstrike_money",0) != stmne then
-		ply:SetNW2Int("cstrike_money",stmne)
+	local maxmoney = GetConVar("mp_maxmoney"):GetInt()
+	if ply:GetNW2Int("cstrike_money",0) != math.min(stmne,maxmoney) then
+		ply:SetNW2Int("cstrike_money",math.min(stmne,maxmoney))
 	end
 end)
 
 -- Kill points related
 function RewardPlayer(victim, attacker, amount)
 	local curmoney = attacker:GetNW2Int("cstrike_money",0)
+	local maxmoney = GetConVar("mp_maxmoney"):GetInt()
 	local finalreward
 	if victim != attacker and attacker:IsPlayer() then 
 		finalreward = curmoney + amount
-		attacker:SetNW2Int("cstrike_money",finalreward)
+		attacker:SetNW2Int("cstrike_money",math.min(finalreward,maxmoney))
 	end
 end
 
